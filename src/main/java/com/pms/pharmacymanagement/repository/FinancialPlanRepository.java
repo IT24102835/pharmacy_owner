67c3c50a8ad54f903a1dd72744f6130335abf547
IT24102835 <IT24102835@my.sliit.lk>
2025-09-28 19:23:27 +0530

    Delete src/main directory

--- src/main/java/com/pms/pharmacymanagement/repository/FinancialPlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pms.pharmacymanagement.repository;
-
-import com.pms.pharmacymanagement.model.FinancialPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FinancialPlanRepository extends JpaRepository<FinancialPlan, Long> {
-}
--- src/main/java/com/pms/pharmacymanagement/repository/PaymentConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pms.pharmacymanagement.repository;
-
-import com.pms.pharmacymanagement.model.PaymentConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentConfigRepository extends JpaRepository<PaymentConfig, Long> {
-}
